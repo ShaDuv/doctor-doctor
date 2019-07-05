e0e382a4ead622c6e73cfb0b92b29e160057fb23
Shawna Duvall <shawnaduvall@gmail.com@>
2019-07-05 09:45:19 -0700

    trying to figure out how to get the variable of the key to pass in when it is called exports instead of export and is not a js file, confused by this unexplained change in practice

--- src/search.js
+++ src/search.js
@@ -1,8 +1,9 @@
+import 
 export class DoctorSearch {
   getDoctorByName(name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = ``;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=42f6241b1440af1209b0db153f0a58de`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
