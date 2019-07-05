2a179182290b8a248f6ec68c943a73bc3d2b7c16
Shawna Duvall <shawnaduvall@gmail.com@>
2019-07-05 10:14:18 -0700

    redid api to to actually match lessons taugh rather than the exports. thing that makes it easer to grade because it keeps breaking and doesn't seem to want to recognize the exports call.

--- src/search.js
+++ src/search.js
@@ -1,9 +1,11 @@
-import 
+const apiKey = process.env.API_KEY
+
+
 export class DoctorSearch {
   getDoctorByName(name) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=42f6241b1440af1209b0db153f0a58de`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 
 module.exports = {
   entry: './src/main.js',
@@ -14,6 +16,7 @@ module.exports = {
     contentBase: './dist'
   },
   plugins: [
+    new Dotenv(),
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new CopyPlugin([
