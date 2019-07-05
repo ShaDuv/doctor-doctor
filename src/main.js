cfb1612807fb007d56a7682df48ef4629c7dc067
Shawna Duvall <shawnaduvall@gmail.com@>
2019-07-05 16:12:47 -0700

    still working

--- src/main.js
+++ src/main.js
@@ -13,7 +13,9 @@ $(document).ready(function() {
     $('#symptom').val("");
 
     const profileTemplate = document.getElementById("profileTemplate").innerHTML;
+    const practiceTemplate = document.getElementById("practiceTemplate").innerHTML;
     let profileDisplay = '';
+    let practiceDisplay = '';
 
     let doctorSearch = new DoctorSearch();
     let nameSearchResult = doctorSearch.getDoctorByName(name, symptom);
@@ -26,21 +28,20 @@ $(document).ready(function() {
       for (var i = 0; i < body.data.length; i++) {
         let profile = new DoctorProfile(body.data[i]);
         profileDisplay += profileTemplate.replace(/{{firstName}}/g, profile.firstName)
-                                         .replace(/{{firstName}}/g, profile.lastName)
-                                         .replace(/{{website}}/g, profile.website)
-                                         .replace(/{{phone}}/g, profile.phone)
-
+                                         .replace(/{{lastName}}/g, profile.lastName)
         for (var p = 0; p < profile.practice.length; p++) {
           let practice = profile.practice[p];
-          $('.website').text(practice.website);
+          profileDisplay += practiceTemplate.replace(/{{street}}/g, practice.street)
+                                           .replace(/{{city}}/g, practice.city)
+                                           .replace(/{{state}}/g, practice.state)
+                                           .replace(/{{zip}}/g, practice.zip)
+                                           .replace(/{{phone}}/g, practice.phone)
+                                           .replace(/{{accepting}}/g, practice.accepting);
 
-        }
-        // $('.location').text(profile.lastName);
-        // $('.accepting-patients').text(profile.lastName);
-        // $('.phone').text(profile.lastName);
-      }
-
-    }, function(error) {
+        };
+      };
+      document.getElementById("displayName").innerHTML = profileDisplay;
+    },function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
